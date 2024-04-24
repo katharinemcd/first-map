7f2fed5ea8bb6d9d9d9611e7a58d63de6b90104f
katharinemcd <mcdaniel.katharine@gmail.com>
2024-04-23 23:33:50 -0400

    Create map.py

---
+++ map.py
@@ -0,0 +1,12 @@
+import folium
+from folium.plugins import MarkerCluster
+import pandas as pd
+
+boulder_coordinates = [40.015, -105.2705]
+
+my_map = folium.Map(location = boulder_coordinates, zoom_start = 13)
+
+my_map
+
+
+
