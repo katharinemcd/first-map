6a54c650d164265fb9e02ae6941211f755e3f276
katharinemcd <mcdaniel.katharine@gmail.com>
2024-04-24 12:42:57 -0400

    Create boulder-ex.py

---
+++ boulder-ex.py
@@ -0,0 +1,19 @@
+import folium
+from folium.plugins import MarkerCluster
+import pandas as pd
+
+boulder_coordinates = [40.015, -105.2705]
+
+my_map = folium.Map(location = boulder_coordinates, zoom_start = 13)
+map = folium.Map()
+
+map
+my_map
+
+print("test")
+
+#pip3 install folium
+#pip3 install pandas
+
+
+
