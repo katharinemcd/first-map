fa79b13bf53c087f86886a38e42c543a096e8c0b
katharinemcd <mcdaniel.katharine@gmail.com>
2024-04-24 13:12:53 -0400

    yayyy open the html link with live server

--- boulder-ex.py
+++ boulder-ex.py
@@ -1,14 +1,16 @@
 import folium
-from folium.plugins import MarkerCluster
-import pandas as pd
+#from folium.plugins import MarkerCluster
+#import pandas as pd
 
 boulder_coordinates = [40.015, -105.2705]
 
 my_map = folium.Map(location = boulder_coordinates, zoom_start = 13)
-map = folium.Map()
+#map = folium.Map()
 
-map
-my_map
+#map
+my_map.save("map.html")
+#lPython.display import display
+#display(my_map)
 
 print("test")
 
